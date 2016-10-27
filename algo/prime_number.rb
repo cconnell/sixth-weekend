68cddbdbe1cbb28a44385abd38eaee555f80f83b
colleenmcguckin <colleenmcguckin@gmail.com>
2016-10-27 11:17:16 -0700

    fill in highest prime, refactor is_prime?

--- algo/prime_number.rb
+++ algo/prime_number.rb
@@ -4,16 +4,14 @@
 # true if the number is prime and false if it is not. A prime number is a number that is only
 # divisible by itself and the number 1.
 
-def is_prime?(number)
-  return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each { |i| return false if num % i == 0 }
-  true
+def is_prime? num
+  num > 1 && !Math.sqrt(num).to_i.downto(2).find { |i| num % i == 0 }
 end
 
 # Second, implement the highest_prime_number_under method below. It should accept a number as
 # an argument and return the highest prime number under that number. For example, the
 # highest prime number under 10 is 7.
 
-def highest_prime_number_under(number)
-
+def highest_prime_number_under number
+  number.pred.downto(2).find { |i| is_prime? i }
 end
